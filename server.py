5f69cc4f6cac808b6615dc138c0ba7035db1a9e3
gushono <gustavo.honoratonic@gmail.com>
2023-06-22 17:37:28 -0300

    feature: changing port used to run app and validate envs before start server

--- server.py
+++ server.py
@@ -1,10 +1,33 @@
+import os
+
 from fastapi import FastAPI
 
 from src.database.database import init_db
 from src.entrypoints.routers.router import add_routes
 
+REQUIRED_ENV_VARIABLES = [
+    "WEATHER_MAP_API_KEY",
+    "TWITTER_BEARER_TOKEN",
+    "TWITTER_CONSUMER_KEY",
+    "TWITTER_CONSUMER_SECRET",
+    "TWITTER_ACCESS_TOKEN",
+    "TWITTER_ACCESS_TOKEN_SECRET"
+]
+
+
+def validate_environment_variables():
+    missing_variables = []
+    for var in REQUIRED_ENV_VARIABLES:
+        if var not in os.environ:
+            missing_variables.append(var)
+
+    if missing_variables:
+        raise Exception(f"Missing environment variables: {missing_variables}")
+
 
 def create_app(debug_mode: bool) -> FastAPI:
+    validate_environment_variables()
+
     app = FastAPI()
     add_routes(app)
     init_db(app)
